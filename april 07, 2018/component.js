8d67c74d43292bd2c908d494eb06fb48549953bb
CS-MKTG-JAGO-News-Web-Web Developer-Shahali <182111@prangroup.com>
2019-04-08 08:52:20 +0600

    practice file uploaded:

---
+++ april 07, 2018/component.js
@@ -0,0 +1,16 @@
+Vue.component('sb',{
+    template : "<p>SB component</p>",
+});
+
+Vue.component('shahali',{
+    template : "<p>Shahali Component</p>",
+});
+
+Vue.component('bogdadi',{
+    template : "<p>Bogdadi Component</p>",
+});
+
+Vue.component('another-component',{
+    template : "<p>Another Component</p>",
+});
+
